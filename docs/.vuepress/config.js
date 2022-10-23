171705565f3a9fbf330b5180c18e0734bdc7e067
LiZhengHao <1263212577@qq.com>
2022-10-23 17:18:51 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,27 +1,27 @@
 module.exports = {
-    title: 'AspNetCore.StartupTemplate',
-    description: 'AspNetCore Webapi项目快速启动模板',
-    themeConfig: {
-        logo: '/logo-min.png',
-        nav: [
-            { text: 'Github', link: 'https://github.com/li-zheng-hao/AspNetCore.StartupTemplate', target: '_blank' }
-        ],
-        sidebar: [
-            {
-                title: 'Group 1',
-                path: '/foo/',
-                collapsable: false,
-                sidebarDepth: 1,
-                children: [
-                    '/'
-                ]
-            },
-            {
-                title: 'Group 2',
-                children: [ /* ... */],
-                initialOpenGroupIndex: -1
-            }
-        ],
-
-    }
-}
+  title: "AspNetCore.StartupTemplate",
+  description: "AspNetCore Webapi项目快速启动模板",
+  plugins: ["@vuepress/back-to-top"],
+  lastUpdated: true,
+  themeConfig: {
+    logo: "/logo-min.png",
+    nav: [
+      {
+        text: "Github",
+        link: "https://github.com/li-zheng-hao/AspNetCore.StartupTemplate",
+        target: "_blank",
+      },
+    ],
+    sidebar: {
+      "/guide/": [
+        "",
+        "模型验证",
+        "依赖注入",
+        "定时任务",
+        "雪花ID",
+        "事件总线",
+        "ORM",
+      ],
+    },
+  },
+};
--- docs/README.md
+++ docs/README.md
@@ -1,15 +1,16 @@
 ---
 home: true
 heroText: AspNetCore Webapi项目快速启动模板
+heroImage: /logo.png
 actionText: 快速上手 →
-actionLink: /zh/guide/
+actionLink: /guide/
+colorModeSwitch: true
 features:
-- title: 开箱即用
-  details: 配置好了项目必须的功能，开箱即可开始业务开发
-- title: 规范
-  details: 各个功能均使用最流行、最通用的第三方库，如Swagger、Mapster、Autofac
-- title: 上手快
-  details: 各种功能均有对应示例代码，上手难度低
+  - title: 开箱即用
+    details: 配置好了项目必须的功能，开箱即可开始业务开发
+  - title: 规范
+    details: 各个功能均使用最流行、最通用的第三方库，如Swagger、Mapster、Autofac
+  - title: 上手快
+    details: 各种功能均有对应示例代码，上手难度低
 footer: MIT Licensed | Copyright © 2022- Create By 李正浩
 ---
-
